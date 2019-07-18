c822da595b3e8f613997207180085216490848cc
namratachaudhary <chdhry.namrata@gmail.com>
2019-07-18 13:46:15 +0200

    remove field selector

--- pkg/controller/oneagent/oneagent_controller.go
+++ pkg/controller/oneagent/oneagent_controller.go
@@ -15,7 +15,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -262,13 +261,8 @@ func (r *ReconcileOneAgent) reconcileNodesMarkedForDeletion(
 
 	// nodes := &corev1.NodeList(metav1.ListOptions{})
 	nodeList := &corev1.NodeList{}
-	fieldSelector := fields.SelectorFromSet(fields.Set{
-		"spec.unschedulable": "True",
-	})
 	labelSelector := labels.SelectorFromSet(labels.Set(instance.Spec.NodeSelector))
 	listOps := &client.ListOptions{
-		//--field-selector=spec.unschedulable=True
-		FieldSelector: fieldSelector,
 		LabelSelector: labelSelector,
 	}
 	err := r.client.List(context.TODO(), listOps, nodeList)
